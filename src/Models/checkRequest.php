127a30315eee24ceb62de4a1c08a12cc56b76816
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-06-09 16:15:33 +0300

    data output

--- src/Models/checkRequest.php
+++ src/Models/checkRequest.php
@@ -20,6 +20,8 @@ Class checkRequest {
             $data = $toJson->normalizeJson($data);
             $data = str_replace('\"', '"', $data);
             $data = str_replace('\\"', '"', $data);
+            echo $data;
+            exit();
             $post_data = json_decode($data, true);
 
         }
